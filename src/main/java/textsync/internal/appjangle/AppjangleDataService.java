aacf759ba23ca0f9fae5c07cca6a4bcbeef2d65d
mxro <mxro@nowhere.com>
2014-09-30 10:46:37 +1000

    Fixing textsync, updating APIs. Using Nextweb API instead of onedb API Autocommit Tue Sep 30 10:46:37 EST 2014

--- src/main/java/textsync/internal/appjangle/AppjangleDataService.java
+++ src/main/java/textsync/internal/appjangle/AppjangleDataService.java
@@ -7,19 +7,7 @@ package textsync.internal.appjangle;
 import io.nextweb.Node;
 import io.nextweb.Session;
 import io.nextweb.common.User;
-import one.common.extend.OneExtend;
-import one.core.dsl.CoreDsl;
-import one.core.dsl.callbacks.WhenCommitted;
-import one.core.dsl.callbacks.WhenLoaded;
-import one.core.dsl.callbacks.WhenResponseFromServerReceived;
 import one.core.dsl.callbacks.WhenShutdown;
-import one.core.dsl.callbacks.WhenVersionsCleared;
-import one.core.dsl.callbacks.results.WithCommittedResult;
-import one.core.dsl.callbacks.results.WithLoadResult;
-import one.core.dsl.callbacks.results.WithOperationResult;
-import one.core.dsl.callbacks.results.WithVersionsClearedResult;
-import one.core.nodes.OneNode;
-import one.core.nodes.OneValue;
 import textsync.internal.DataService;
 
 /**
@@ -58,49 +46,24 @@ public class AppjangleDataService implements DataService {
     @Override
     public void createNewNode(final String value, final String title, final String extension, final boolean isPublic, final WhenNewNodeCreated callback) {
 
-       throw new RuntimeException("Not supported.")
+       throw new RuntimeException("Not yet supported.");
 
     }
 
     public void downloadChanges(final String localValue, String nodeUri, final WhenChangesDownloaded callback) {
-       final CoreDsl dsl = session.one();
+      
+    	Node resolvedNode = session.link(nodeUri, user.userNode().secret()).reload().get();
+    	
+    	 String remoteValue = resolvedNode.value(String.class);
 
-        dsl.reload(nodeUri).withSecret(user.userNodeSecret()).in(session).and(new WhenLoaded() {
-
-            @Override
-            public void thenDo(WithLoadResult<Object> wlr) {
-                Object resolvedNode = dsl.dereference(wlr.loadedNode()).in(session);
-
-                if (!(resolvedNode instanceof OneValue<?>)) {
-                    callback.onFailure(new Exception("Node was not of type OneValue: " + wlr.loadedNode()));
-                    return;
-                }
-
-                OneValue<?> valueNode = (OneValue<?>) resolvedNode;
-
-                if (!(valueNode.getValue() instanceof String)) {
-                    callback.onFailure(new Exception("Value nodes value was not of type String: " + valueNode));
-                    return;
-                }
-
-                String remoteValue = (String) valueNode.getValue();
-
-                if (remoteValue.equals(localValue)) {
-                    callback.onUnchanged();
-                    return;
-                }
-                
-                callback.onChanged(remoteValue);
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-                callback.onFailure(t);
-            }
-            
-            
-        });
-        
+         if (remoteValue.equals(localValue)) {
+             callback.onUnchanged();
+             return;
+         }
+         
+         callback.onChanged(remoteValue);
+         
+       
         
     }
 
@@ -109,81 +72,6 @@ public class AppjangleDataService implements DataService {
         callback.thenDo();
     }
 
-    private interface WhenSyncDataNodeAsserted {
-
-        public void thenDo(OneNode syncDataNode);
-
-        public void onFailure(Throwable t);
-    }
-
-    private void assertAjFileSyncDataNode(final WhenSyncDataNodeAsserted callback) {
-
-        CoreDsl dsl = session.one();
-
-       
-        
-        dsl.load(user.userNodeUri()).withSecret(user.userNodeSecret()).in(session).and(new WhenLoaded() {
-
-            @Override
-            public void thenDo(WithLoadResult<Object> wlr) {
-
-                assertChild(wlr.loadedNode(), "apps", new WhenLoaded() {
-
-                    @Override
-                    public void thenDo(WithLoadResult<Object> wlr) {
-
-                        assertChild(wlr.loadedNode(), "textsync", new WhenLoaded() {
-
-                            @Override
-                            public void thenDo(WithLoadResult<Object> wlr) {
-
-                                assertChild(wlr.loadedNode(), "upload", new WhenLoaded() {
-
-                                    @Override
-                                    public void thenDo(WithLoadResult<Object> wlr) {
-                                        callback.thenDo(wlr.loadedNode());
-                                    }
-
-                                    @Override
-                                    public void onFailure(Throwable t) {
-                                        callback.onFailure(t);
-                                    }
-                                });
-
-                            }
-
-                            @Override
-                            public void onFailure(Throwable t) {
-                                callback.onFailure(t);
-                            }
-                        });
-
-                    }
-
-                    @Override
-                    public void onFailure(Throwable t) {
-                        callback.onFailure(t);
-                    }
-                });
-
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-                callback.onFailure(t);
-            }
-        });
-
-
-
-    }
-
-    private void assertChild(final OneNode forNode, final String childPath, final WhenLoaded callback) {
-
-        //final CoreDsl dsl = client.one();
-
-        OneExtend.assertChild(session, forNode, childPath, callback);
-
-
-    }
+   
+   
 }
