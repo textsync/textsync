1654094bf0408bcdc47a64231dc223e8a53e3433
mxro <mxro@nowhere.com>
2014-10-01 10:02:24 +1000

    Changing to asynchronous operations Autocommit Wed Oct 01 10:02:24 EST 2014

--- src/main/java/textsync/internal/appjangle/AppjangleDataService.java
+++ src/main/java/textsync/internal/appjangle/AppjangleDataService.java
@@ -20,86 +20,89 @@ import textsync.internal.DataService;
  */
 public class AppjangleDataService implements DataService {
 
-    private final Session session;
-    private final User user;
+	private final Session session;
+	private final User user;
 
-    public AppjangleDataService(Session session, User user) {
-        this.session = session;
-        this.user = user;
-    }
+	public AppjangleDataService(Session session, User user) {
+		this.session = session;
+		this.user = user;
+	}
 
-    public void uploadChanges(final String newValue, String nodeUri, final WhenChangesUploaded callback) {
-    	Query query = session.link(nodeUri, user.userNode().secret()).reload();
+	public void uploadChanges(final String newValue, String nodeUri,
+			final WhenChangesUploaded callback) {
+		Query query = session.link(nodeUri, user.userNode().secret()).reload();
 
-    	query.catchExceptions(new ExceptionListener() {
-			
+		query.catchExceptions(new ExceptionListener() {
+
 			@Override
 			public void onFailure(ExceptionResult arg0) {
 				callback.onFailure(arg0.exception());
 			}
 		});
-    	
-        String oldValue = resolvedNode.value(String.class);
 
-        if (oldValue.equals(newValue)) {
-            callback.thenDo(false);
-            return;
-        }
+		query.get(new Closure<Node>() {
 
-        resolvedNode.setValue(newValue).get();
-        
-        resolvedNode.clearVersions(3).get();
-        
-        callback.thenDo(true);
-       
+			@Override
+			public void apply(Node resolvedNode) {
+				String oldValue = resolvedNode.value(String.class);
 
-    }
+				if (oldValue.equals(newValue)) {
+					callback.thenDo(false);
+					return;
+				}
 
-    @Override
-    public void createNewNode(final String value, final String title, final String extension, final boolean isPublic, final WhenNewNodeCreated callback) {
+				resolvedNode.setValue(newValue).get();
 
-       throw new RuntimeException("Not yet supported.");
+				resolvedNode.clearVersions(3).get();
 
-    }
+				callback.thenDo(true);
+			}
+		});
 
-    public void downloadChanges(final String localValue, String nodeUri, final WhenChangesDownloaded callback) {
-      
-    	Query query = session.link(nodeUri, user.userNode().secret()).reload();
-    	
-    	query.catchExceptions(new ExceptionListener() {
-			
+	}
+
+	@Override
+	public void createNewNode(final String value, final String title,
+			final String extension, final boolean isPublic,
+			final WhenNewNodeCreated callback) {
+
+		throw new RuntimeException("Not yet supported.");
+
+	}
+
+	public void downloadChanges(final String localValue, String nodeUri,
+			final WhenChangesDownloaded callback) {
+
+		Query query = session.link(nodeUri, user.userNode().secret()).reload();
+
+		query.catchExceptions(new ExceptionListener() {
+
 			@Override
 			public void onFailure(ExceptionResult arg0) {
 				callback.onFailure(arg0.exception());
 			}
 		});
-    	
-    	query.get(new Closure<Node>() {
-			
+
+		query.get(new Closure<Node>() {
+
 			@Override
 			public void apply(Node resolvedNode) {
 				String remoteValue = resolvedNode.value(String.class);
 
-		         if (remoteValue.equals(localValue)) {
-		             callback.onUnchanged();
-		             return;
-		         }
-		         
-		         callback.onChanged(remoteValue);
+				if (remoteValue.equals(localValue)) {
+					callback.onUnchanged();
+					return;
+				}
+
+				callback.onChanged(remoteValue);
 			}
 		});
-    	
-    	 
-         
-       
-        
-    }
 
-    public void shutdown(WhenShutdown callback) {
-        //session.close().get();
-        callback.thenDo();
-    }
+	}
 
-   
-   
+	public void shutdown(WhenShutdown callback) {
+		// session.close().get();
+		callback.thenDo();
+	}
+
 }
